d95ddca7dc81a324acc4675ada23e61aa9b1ee27
Cyril234 <cyril_seitz@hotmail.com>
2026-05-26 11:13:50 +0200

    change url from mogodb for production

--- src/main/java/ch/bbw/chattrix/chat/controller/ChatController.java
+++ src/main/java/ch/bbw/chattrix/chat/controller/ChatController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = "${FRONTEND_URL}", allowCredentials = "true")
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
