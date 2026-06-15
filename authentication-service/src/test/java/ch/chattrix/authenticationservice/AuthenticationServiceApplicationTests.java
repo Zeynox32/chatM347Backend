f77d05c493d40b508f6fea7b4294b77d71d918ae
loris-sigrist <186710904+loris-sigrist@users.noreply.github.com>
2026-06-15 11:06:37 +0200

    mock rabbitmq to make default test green

--- authentication-service/src/test/java/ch/chattrix/authenticationservice/AuthenticationServiceApplicationTests.java
+++ authentication-service/src/test/java/ch/chattrix/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -1,15 +1,22 @@
 package ch.chattrix.authenticationservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
 @ActiveProfiles("test")
 class AuthenticationServiceApplicationTests {
+
+    @MockBean
+    private ConnectionFactory connectionFactory;
+
+    @MockBean
+    private RabbitTemplate rabbitTemplate;
 
     @Test
     void contextLoads() {
     }
-
 }
--- gateway-service/src/test/java/ch/chattrix/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/ch/chattrix/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,15 +1,23 @@
 package ch.chattrix.gatewayservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class GatewayServiceApplicationTests {
 
+	@MockBean
+	private ConnectionFactory connectionFactory;
+
+	@MockBean
+	private RabbitTemplate rabbitTemplate;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
